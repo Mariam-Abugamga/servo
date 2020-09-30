535056c58a406b453056f04399e4254cf199ae0e
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-09-30 14:38:06 -0400

    moves the servo back and forth and added comment

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
+// created by; Mariam Abugamga
+// date; sept 30, 2020
+// this program moves the servo back and forth
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    robotbit.Servo(robotbit.Servos.S1, 0)
+    basic.pause(1000)
+    robotbit.Servo(robotbit.Servos.S1, 90)
+    basic.pause(1000)
 })
